f4d8705c1e8983154114066ae50251a26b9df7f6
candicerodrigues <candice.anania@icloud.com>
2024-02-09 12:23:54 +0100

    ajszgjaz
    gwezgdjz

--- react-frontend/src/Pages/Editor/EditorPage.jsx
+++ react-frontend/src/Pages/Editor/EditorPage.jsx
@@ -1,11 +1,20 @@
+import React, { useState } from 'react';
+
 const EditorPage = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageSelect = (event) => {
+        const file = event.target.files[0];
+        setSelectedImage(URL.createObjectURL(file));
+    };
 
     return (
         <div>
             <h1>EDITOR</h1>
+            <input type="file" accept="image/*" onChange={handleImageSelect} />
+            {selectedImage && <img src={selectedImage} alt="Selected Image" />}
         </div>
     );
-
 };
 
 export default EditorPage;
