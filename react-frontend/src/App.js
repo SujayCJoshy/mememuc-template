3dce0a5549e39d494661d6989d9fecdba09d7c4d
Laura.Garcia <99455921+lcocooo@users.noreply.github.com>
2024-02-09 12:46:51 +0100

    React-router-dom install

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import LandingPage from './Pages/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
 function App() {
   return (
     <>
-      <LandingPage></LandingPage>
+      <LandingPage>
+           </LandingPage>
     </>
   );
 }
